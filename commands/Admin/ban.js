ec8819881a689a34245c006ae1e9266e4e34e960
KarimX32 <ikakarim9@gmail.com>
2021-01-13 12:08:53 +0200

    COVID CMD

---
+++ commands/Admin/ban.js
@@ -0,0 +1,53 @@
+const discord = require('discord.js');
+
+module.exports.run = async (Client, message, args, prefix) => {
+
+    // the perm. that the member need it to ban someone
+    if(!message.member.hasPermission('BAN_MEMBERS', 'ADMINISTRATOR'))
+    // if someone dont hv perm it will send this message
+    message.channel.send("You don't have permission to use that command.");
+
+    else {
+      if (!message.guild) return;
+  
+      const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+  
+      if (user) {
+  
+        const member = message.guild.member(user);
+  
+        if (member) {
+  
+          member
+
+          // banning code 
+            .ban({
+                // the reason
+              reason: 'They were bad!',
+            })
+            .then(() => {
+            // it will send this message once the person is banned
+              message.reply(`Successfully banned`);
+            })
+            // log err in the console
+            .catch(err => {
+              // if the bot wasnt able to ban the member bcz he hv a higher role it will not ban him and if the bot dont hv to perm it will not ban him and send this messge
+              message.reply('I was unable to ban the member');
+  
+              console.error(err);
+            });
+        } else {
+          // if the member isnt in the server and u typed e.g. =ban @karimx it will send this message
+          message.reply("That user isn't in this guild!");
+        }
+      } else {
+       // if u typed =ban without mentioning some1 it will send this message
+        message.reply("You didn't mention the user to ban!");
+      }
+  };
+}
+
+module.exports.help = {
+    name: `ban`,
+    aliases: []
+};
---
+++ commands/Admin/unmute.js
@@ -0,0 +1,26 @@
+const discord = require('discord.js');
+
+module.exports.run = async (Client, message, args, prefix) => {
+
+
+// this code check if the member have the perm to unmute or the bot hv the perm to unmute ppl
+    if(!message.member.hasPermission('ADMINISTRATOR')) return;
+let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
+if(member.hasPermission(['ADMINISTRATOR']) && !message.member.hasPermission('ADMINISTRATOR')) return;
+
+//it reorgnize the unmuted role and the member and add the unmuted and remove the muted role
+    let mutedRole = message.guild.roles.cache.get('729068722916032522'); // u put the muted role ID
+    let verifiedRole = message.guild.roles.cache.get('729666531067428935'); // the member role ID
+    if(mutedRole) {
+        member.roles.remove(mutedRole);
+        member.roles.add(verifiedRole);
+// it will send this message once the bot unmuted the member
+        message.channel.send(`I unmuted ${member}😂`);
+    }
+
+}
+
+module.exports.help = {
+    name: `unmute`,
+    aliases: []
+};
